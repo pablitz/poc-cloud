a19122a4cf963e95e33af69fb9d01043d387a4c6
pablo godoy de albuquerque <pablo-albuquerque@pablo.albuquerque>
2021-09-20 13:56:48 -0300

    adicionando swagger

--- restkafka/src/main/java/br/com/pga/restkafka/Application.java
+++ restkafka/src/main/java/br/com/pga/restkafka/Application.java
@@ -2,7 +2,8 @@ package br.com.pga.restkafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+@EnableSwagger2
 @SpringBootApplication
 public class Application {
 
