34d1869b5ad79a2702e13070f1b2c2f33e7bdaf1
servidor2 <pablogodoyalbuquerque@gmail.com>
2021-09-27 15:03:05 -0300

    mudanças nos testes

--- restkafka/src/test/java/br/com/pga/restkafka/PersonServiceIntegrationTest.java
+++ restkafka/src/test/java/br/com/pga/restkafka/PersonServiceIntegrationTest.java
@@ -1,49 +1,63 @@
 package br.com.pga.restkafka;
 
+import br.com.pga.restkafka.faker.PersonFactory;
 import br.com.pga.restkafka.model.Person;
 import br.com.pga.restkafka.repository.PersonRepository;
 import br.com.pga.restkafka.services.PersonService;
 import com.github.javafaker.Faker;
-import org.junit.internal.runners.statements.ExpectException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.orm.jpa.JpaObjectRetrievalFailureException;
-import org.springframework.test.context.jdbc.Sql;
 
-import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
-
-import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Transactional
 public class PersonServiceIntegrationTest {
 
-
     Faker faker = new Faker();
 
-    String firstName=faker.name().firstName();
+    PersonFactory personFactory = new PersonFactory(faker);
 
-    String lastName=faker.name().lastName();
-
-    String email =faker.internet().safeEmailAddress();
-
-    Integer age =faker.random().nextInt(1,99);
-
-    Person person = new Person(3L,firstName,lastName,age,email, LocalDateTime.now(),LocalDateTime.now());
-
+    @Autowired
+    PersonRepository personRepository;
 
     @Autowired
     PersonService personService;
 
+    @BeforeEach
+    @AfterEach
+    public void clean(){
+        personRepository.deleteAll();
+        personRepository.flush();
+    }
+
+
+
     @Test
     public void getAllTest(){
-        assertEquals("jose",personService.findAll().get(0).getFirstName()) ;
-        assertEquals("joao",personService.findAll().get(1).getFirstName()) ;
+
+
+        List<Person> people = Arrays.asList(personFactory.build(), personFactory.build(), personFactory.build());
+
+        personRepository.saveAll(people);
+
+        assertEquals(people.get(0).getFirstName(),personService.findAll().get(0).getFirstName()) ;
+
+        assertEquals(people.get(1).getFirstName(),personService.findAll().get(1).getFirstName()) ;
+
+        assertEquals(people.get(2).getFirstName(),personService.findAll().get(2).getFirstName()) ;
 
     }
 
@@ -51,7 +65,11 @@ public class PersonServiceIntegrationTest {
     @Test
     public void getByIDTest(){
 
-        assertEquals("jose",personService.findById(1L).getFirstName()) ;
+        Person person = personFactory.build();
+
+        personRepository.save(person);
+
+        assertEquals(person.getFirstName(),personService.findById(3L).getFirstName()) ;
 
     }
 
@@ -59,7 +77,11 @@ public class PersonServiceIntegrationTest {
     @Test
     public void putTest(){
 
-        assertEquals(firstName,personService.create(person).getFirstName()) ;
+        Person person = personFactory.build();
+
+        assertEquals(person.getFirstName(),personService.create(person).getFirstName()) ;
+
+        assertEquals(person.getFirstName(),personRepository.findAll().get(0).getFirstName());
 
     }
 
@@ -67,9 +89,41 @@ public class PersonServiceIntegrationTest {
     @Test
     public void deleteTest(){
 
-        personService.remove(1L);
-        assertThrows(JpaObjectRetrievalFailureException.class,()->personService.findById(1L));
+        Person person = personFactory.build();
 
+        personRepository.save(person);
+
+        assertEquals(person.getFirstName(),personRepository.findAll().get(0).getFirstName());
+
+        Long id = personRepository.findAll().get(0).getId();
+
+        personService.remove(personRepository.findAll().get(0).getId());
+
+        assertThrows(JpaObjectRetrievalFailureException.class,()->personService.findById(id));
+
+
+    }
+
+
+
+    @Test
+    public  void update(){
+
+        Person person = personFactory.build();
+
+        personRepository.save(person);
+
+        Person person1 = personFactory.build();
+
+        Long id = personRepository.findAll().get(0).getId();
+
+        person1.setId(id);
+
+        Person person2 = personService.update(person1);
+
+        assertEquals(person2.getFirstName(),person1.getFirstName());
+
+        assertEquals(person2.getFirstName(),personRepository.findById(id).get().getFirstName());
 
     }
 
